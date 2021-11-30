848f99955db876235bcb2de7ba3f51a38452c37f
Hyunsoo Bang <suusuu16@ewhain.net>
2021-12-01 02:38:41 +0900

    Category

---
+++ src/images.js
@@ -0,0 +1,21 @@
+import SubmitBtn from '../assets/outline_task_alt_black_24.png';
+import UnBox from '../assets/outline_check_box_outline_blank_black_24.png';
+import Box from '../assets/outline_check_box_black_24.png';
+import Delete from '../assets/outline_delete_forever_black_24.png';
+import Edit from '../assets/outline_edit_black_24.png';
+import Add from '../assets/add.png';
+import Off from '../assets/off.png';
+import On from '../assets/on.png';
+
+
+export const images = {
+    submit: SubmitBtn,
+    uncompleted: UnBox,
+    completed:Box,
+    delete: Delete,
+    edit: Edit,
+    add: Add,
+    off: Off,
+    on: On,
+
+};
---
+++ src/styles.js
@@ -0,0 +1,53 @@
+import { StyleSheet } from 'react-native';
+import {theme} from './theme';
+
+export const viewStyles=StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: theme.background,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    card:{
+        backgroundColor: '#fff',
+        flex:1,
+        borderRadius:10,
+        margin:20,
+        width:340,
+    },
+    box: {
+        marginLeft:290,
+        flexDirection: 'row',
+    },
+    box2:{
+        marginLeft:240,
+        flexDirection: 'row',
+    }
+    
+});
+
+export const textStyles = StyleSheet.create({
+    title: {
+        fontSize:30,
+        fontWeight: '600',
+        color: theme.main,
+        alignItems: 'flex-start',
+        marginTop: 10,
+        marginLeft: 0,
+    },
+    main: {
+        fontSize:17,
+        fontWeight: '500',
+        color: theme.main,
+        alignItems: 'flex-start',
+        marginTop:20,
+        marginLeft: 20,
+    },
+    
+});
+
+export const barStyles = StyleSheet.create({
+    statusbar: {
+        backgroundColor: theme.background,
+    },
+});
