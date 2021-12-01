8b7a33eb8374a7209ebec5d7020051cb9e150280
dasoya <ekth020216@gmail.com>
2021-12-01 12:00:09 +0900

    modify app

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import App from './src/Montly';
-import React from 'react';
-import RootStack from './src/J_index.js';
+// import React from 'react';
+// import RootStack from './src/J_index.js';
 
-import { NavigationContainer } from '@react-navigation/native';
+// import { NavigationContainer } from '@react-navigation/native';
 
-class App extends React.Component {
+// class App extends React.Component {
 
-  render() {
-    return (
-      <NavigationContainer>
-        <RootStack />
-      </NavigationContainer>
-    );
-  }
+//   render() {
+//     return (
+//       <NavigationContainer>
+//         <RootStack />
+//       </NavigationContainer>
+//     );
+//   }
 
-}
+// }
 
 export default App;
