015bceb6d14e83b4ae7d4e25e520da35fd79cedf
yhjune <yhjune09@gmail.com>
2021-12-05 18:07:59 +0900

    ADD signup screens
    ADD signup screens

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,5 +1,6 @@
 const prefix = 'https://firebasestorage.googleapis.com/v0/b/liffic.appspot.com/o';
 
 export const images ={
-    logo:'${prefix}/icon.png?alt=media',
+    logo: `${prefix}/icon.png?alt=media`,
+    photo: `${prefix}/icon.png?alt=media`,
 };
