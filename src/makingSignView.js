9c05216cdc86e75ec0f19e81a943959864e990d4
dasoya <ekth020216@gmail.com>
2021-12-06 16:39:08 +0900

    1206

--- src/makingSignView.js
+++ src/makingSignView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet,StatusBar,SafeAreaView, Text, View, Keyboard ,Button,BackHandler } from 'react-native';
+import { StyleSheet,StatusBar,SafeAreaView, Text, View, Keyboard ,Button,BackHandler,Alert } from 'react-native';
 import { viewStyles, textStyles,  iconStyles } from './styles';
 import EventInput from './EventInput';
 import { TouchableWithoutFeedback } from 'react-native';
@@ -22,7 +22,7 @@ const makeSign= ({ navigation, route }) => {
         <SafeAreaView style={viewStyles.container}>
           <StatusBar barStyle="light-content" style={textStyles.statusBar} />
           <View style={viewStyles.header}>
-            <IconButton icon={images.back} onPress={ () => {goBack({navigation});}}/>
+            <IconButton icon={images.back} onPress={() => {back({navigation});}}/>
           </View>
           <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
           <View style={viewStyles.content}>
@@ -36,11 +36,8 @@ const makeSign= ({ navigation, route }) => {
           
           <View style={viewStyles.footer}>
             <View>
-            {/* <Button
-      title="Go to Jane's profile"
-      onPress={() => navigation.navigate('showSign')}
-    /> */}
-              <IconButton icon={images.done} onPress={() => navigation.navigate('showSign') }/>
+          
+              <IconButton icon={images.done} onPress={() =>{confirm({navigation});}}/>
             </View>
           </View>
 
@@ -50,6 +47,50 @@ const makeSign= ({ navigation, route }) => {
     
   );
 }
+
+function back({navigation}){
+
+  Alert.alert(                    // 말그대로 Alert를 띄운다
+    "Quit editing?",                    // 첫번째 text: 타이틀 제목
+    "If you leave before saving, your changes will be lost.",    
+    [                              // 버튼 배열
+      {
+        text: "cancel",                              // 버튼 제목
+        onPress: () => console.log("cancel"),     //onPress 이벤트시 콘솔창에 로그를 찍는다
+        style: "cancel"
+      },
+      { text: "confirm", onPress: () => goBack({navigation}) }, //버튼 제목
+
+    ],
+    { cancelable: false }
+  )
+
+  
+
+}
+
+function confirm({navigation}){
+
+  Alert.alert(                    // 말그대로 Alert를 띄운다
+
+    "saved it successfully",
+    "",  
+   
+    [
+      { text: "OK", onPress:() => navigation.navigate('showSign')}, //버튼 제목
+      
+    ],
+    { cancelable: false }
+    
+  )
+
+  
+
+}
+
+
+
+
 
 
 
